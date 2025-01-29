4b8e73715414c7f170a942218334e8f36cf6b1ea
KevinLatino <latinokevin9@gmail.com>
2025-01-29 09:02:35 -0600

    feat: adding paymentlink CRUD

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import PaymentRoute from './routes/ParymentLink.routes';
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
-app.use(cors())
-export default app
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json()); 
+app.use("/paymentlink", PaymentRoute)
+
+export default app;
