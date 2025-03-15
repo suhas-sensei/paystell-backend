10e3a9876f3d9675ec4f9b705ac4e64f44a4f980
MPSxDev <mparedes.digital@gmail.com>
2025-03-15 19:37:14 -0300

    chore: add explicit Request and Response type assertions for user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { UserController } from '../controllers/UserController';
 
 const router = Router();
 const userController = new UserController();
 
-router.post('/', (req, res) => userController.createUser(req, res)); 
-router.get('/:id', async (req, res) => userController.getUserById(req, res)); 
-router.put('/:id', (req, res) => userController.updateUser(req, res)); 
-router.delete('/:id', (req, res) => userController.deleteUser(req, res)); 
-router.get('/', (req, res) => userController.getAllUsers(req, res)); 
+router.post('/', (req, res) => userController.createUser(req as Request, res as Response)); 
+router.get('/:id', async (req, res) => userController.getUserById(req as Request, res as Response)); 
+router.put('/:id', (req, res) => userController.updateUser(req as Request, res as Response)); 
+router.delete('/:id', (req, res) => userController.deleteUser(req as Request, res as Response)); 
+router.get('/', (req, res) => userController.getAllUsers(req as Request, res as Response)); 
 
 export default router;
