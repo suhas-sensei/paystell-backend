880df09a7f00aa8b273e361ddeffb6d34bb919fb
Renzo Espinosa <renzoespinosa.b@gmail.com>
2025-03-15 20:30:58 +0000

    chore types add type definitions for test utilities

---
+++ src/tests/types.ts
@@ -0,0 +1,6 @@
+export interface MockFunction<T = unknown, R = unknown> {
+  (...args: T[]): R;
+  mockImplementation: (fn: (...args: T[]) => R) => void;
+  mockResolvedValue: (value: R) => void;
+  mockRejectedValue: (error: Error) => void;
+} 
