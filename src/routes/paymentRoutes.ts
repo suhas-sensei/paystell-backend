ebb43584a6df0d74dd09a610f10264dbc0a641e6
MPSxDev <mparedes.digital@gmail.com>
2025-03-15 19:37:12 -0300

    chore: add RequestHandler type for payment controller binding

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -1,10 +1,10 @@
-import express from "express"
+import express, { RequestHandler } from "express"
 import { PaymentController } from "../controllers/PaymentController"
 
 const router = express.Router()
 const paymentController = new PaymentController()
 
-router.post("/", paymentController.createPayment.bind(paymentController))
+router.post("/", paymentController.createPayment.bind(paymentController) as RequestHandler)
 
 export { router as paymentRouter }
 
