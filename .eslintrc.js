4b335e60fd4252fa236ff7221532ae13193bda5f
Renzo Espinosa <renzoespinosa.b@gmail.com>
2025-03-15 20:30:52 +0000

    chore lint add ESLint config file to enforce code style rules

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_'
+    }]
+  }
+}; 
