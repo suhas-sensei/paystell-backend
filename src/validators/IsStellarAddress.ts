289fe9097ed12b86f6cdb7dcab48fe0bcbdbce66
Renzo Espinosa <renzoespinosa.b@gmail.com>
2025-03-15 20:31:30 +0000

    fix IsStellarAddress change 'Object' to 'object' and replace 'any' with 'unknown'

--- src/validators/IsStellarAddress.ts
+++ src/validators/IsStellarAddress.ts
@@ -1,14 +1,14 @@
 import { registerDecorator, ValidationOptions } from 'class-validator';
 
 export function IsStellarAddress(validationOptions?: ValidationOptions) {
-    return function (object: Object, propertyName: string) {
+    return function (object: object, propertyName: string) {
         registerDecorator({
             name: 'isStellarAddress',
             target: object.constructor,
             propertyName: propertyName,
             options: validationOptions,
             validator: {
-                validate(value: any) {
+                validate(value: unknown): boolean {
                     return typeof value === 'string' && 
                            value.length === 56 && 
                            value.startsWith('G');
