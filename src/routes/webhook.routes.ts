7a8d2d94443a8b02271cb03bea45fa40d8a51278
Renzo Espinosa <renzoespinosa.b@gmail.com>
2025-03-15 20:31:13 +0000

    chore webhook.routes apply lint fixes and ensure proper type definitions

--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { MerchantController } from '../controllers/merchant.controller'
 import { authenticateMerchant, authenticateStellarWebhook, asyncHandler } from "../middlewares/merchantAuth";
 import { WebhookController } from "../controllers/webhook.controller";
-import { webhookRateLimiter } from "../middlewares/webhookRateLimiter.middleware";
+import { _webhookRateLimiter } from "../middlewares/rateLimiter.middleware";
 
 const router = express.Router();
 const merchantController = new MerchantController();
