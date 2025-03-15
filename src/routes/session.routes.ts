eaf4f8be4f429b59e0b2e28f71ba8587c4219edc
Renzo Espinosa <renzoespinosa.b@gmail.com>
2025-03-15 20:31:12 +0000

    chore session.routes clean up unused imports and resolve lint issues

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -1,12 +1,24 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Response, NextFunction, RequestHandler } from "express";
+import { Request } from "express-serve-static-core";
 import { createSession, deleteSession } from "../controllers/session.controller";
+import { UserRole } from "../enums/UserRole";
 
 const router = express.Router();
 
-const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) => 
-  (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
+interface CustomRequest extends Request {
+  user?: {
+    id: number;
+    email: string;
+    tokenExp?: number;
+    role?: UserRole;
   };
+}
+
+const asyncHandler = (fn: (req: CustomRequest, res: Response, next: NextFunction) => Promise<unknown>): RequestHandler => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req as CustomRequest, res, next)).catch(next);
+  };
+};
 
 router.post("/", asyncHandler(createSession));
 router.delete("/", asyncHandler(deleteSession));
